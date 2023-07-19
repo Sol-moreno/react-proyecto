f203a853961df9de638bcf72308ad071f39b77b2
maria soledad moreno <morenosoledad24@gmail.com>
2023-07-19 01:15:14 -0300

    commit router

--- lePetite/src/components/ItemDetail/ItemDetail.jsx
+++ lePetite/src/components/ItemDetail/ItemDetail.jsx
@@ -1,12 +1,13 @@
 
 
 
+
 const ItemDetail = ({item}) => {
 
     return (
         <div className="container my-5">
             <h2>{item.nombre}</h2>
-            <img src={item.img} alt={item.nombre}/>
+            <img  className='imagencard'src={item.img} alt={item.nombre}/>
             <p>{item.descripcion}</p>
             <p>Precio: ${item.precio}</p>
              <button className="btn btn-primary"> Agregar al Carrito</button>
